5483f327030c2c18f78d347a8e073886edbebee7
YuYue <86163024+YuYue-Amatsuki@users.noreply.github.com>
2024-03-17 23:33:09 +0800

    更新 更新OpenShamrock.py

--- 更新OpenShamrock.py
+++ 更新OpenShamrock.py
@@ -9,7 +9,12 @@ import logging
 import os
 
 url = 'https://nightly.link/whitechi73/OpenShamrock/workflows/build-apk/master?preview'
-search_term = r'https://nightly\.link/whitechi73/OpenShamrock/workflows/build-apk/master/Shamrock.*arm64\.zip'
+
+architectures = {
+    'all': r'https://nightly\.link/whitechi73/OpenShamrock/workflows/build-apk/master/Shamrock.*all\.zip',
+    'arm64': r'https://nightly\.link/whitechi73/OpenShamrock/workflows/build-apk/master/Shamrock.*arm64\.zip',
+    'x86_64': r'https://nightly\.link/whitechi73/OpenShamrock/workflows/build-apk/master/Shamrock.*x86_64\.zip'
+}
 
 # 获取logger实例
 logger = logging.getLogger(__name__)
@@ -28,58 +33,83 @@ response = requests.get(url)
 html_content = response.text
 
 download_links = re.findall(r'href=[\'"](.*?)[\'"]', html_content)
+
+# 提示用户选择 CPU 架构
+print("请选择对应的CPU架构：")
+for idx, architecture in enumerate(architectures.keys()):
+    print(f"{idx+1}. {architecture}")
+
+selected_architecture_idx = int(input("请输入序号："))
+while selected_architecture_idx not in range(1, len(architectures) + 1):
+    print("无效的选择，请重新输入：")
+    selected_architecture_idx = int(input("请输入序号："))
+
+selected_architecture = list(architectures.keys())[selected_architecture_idx - 1]
+
+# 根据选择的 CPU 架构获取对应的下载链接
+selected_download_link = None
 for link in download_links:
-    if re.match(search_term, link):
-        file_request = requests.get(link, stream=True)
-        file_name = link.split('/')[-1]
+    if re.match(architectures[selected_architecture], link):
+        selected_download_link = link
+        break
 
-        # 获取脚本文件的路径
-        script_dir = os.path.dirname(os.path.realpath(__file__))
+if selected_download_link is None:
+    logger.info(f"{datetime.datetime.now()} - 未找到对应的下载链接，请检查选择的 CPU 架构是否正确。")
+    exit()
 
-        # 构建下载文件的保存路径
-        file_path = os.path.join(script_dir, file_name)
+file_request = requests.get(selected_download_link, stream=True)
+file_name = selected_download_link.split('/')[-1]
 
-        # 保存文件并显示下载进度条
-        with open(file_path, 'wb') as f:
-            total_length = int(file_request.headers.get('content-length'))
-            for chunk in tqdm(file_request.iter_content(chunk_size=8192), total=total_length/8192, unit='KB'):
-                if chunk:
-                    f.write(chunk)
+# 获取脚本文件的路径
+script_dir = os.path.dirname(os.path.realpath(__file__))
 
-        logger.info(f"{file_name} 已下载")
+# 构建下载文件的保存路径
+file_path = os.path.join(script_dir, file_name)
 
-        # 解压下载的ZIP文件
-        with zipfile.ZipFile(file_path, 'r') as zip_ref:
-            # 解压到当前脚本所在的路径
-            zip_ref.extractall(script_dir)
-            extracted_files = zip_ref.namelist()  # 获取解压后的文件列表
+# 保存文件并显示下载进度条
+with open(file_path, 'wb') as f:
+    total_length = int(file_request.headers.get('content-length'))
+    for chunk in tqdm(file_request.iter_content(chunk_size=8192), total=round(total_length/8192), unit='KB', bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]', desc=file_name):
+        if chunk:
+            f.write(chunk)
 
-            # 打印解压后的文件路径
-            for extracted_file in extracted_files:
-                extracted_file_path = os.path.join(script_dir, extracted_file)
+logger.info(f"{datetime.datetime.now()} - {file_name} 已下载")
 
-        # 删除原始的ZIP文件
-        os.remove(file_path)
+# 解压下载的ZIP文件
+with zipfile.ZipFile(file_path, 'r') as zip_ref:
+    # 解压到当前脚本所在的路径
+    zip_ref.extractall(script_dir)
+    extracted_files = zip_ref.namelist()  # 获取解压后的文件列表
 
-        # 列出连接到计算机上的所有安卓设备的序号和设备信息
-        adb_devices = subprocess.run(['adb', 'devices'], capture_output=True, text=True).stdout
-        # 检查是否存在连接的设备
-        if "List of devices attached" not in adb_devices:
-            logger.info("未检测到已连接的设备，请连接设备后重新运行程序。")
-        else:
-            devices_list = adb_devices.split('\n')[1:-2]  # 去除开头和结尾多余信息
+    # 打印解压后的文件路径
+    for extracted_file in extracted_files:
+        extracted_file_path = os.path.join(script_dir, extracted_file)
 
-        # 打印设备列表供用户选择
-        for idx, device in enumerate(devices_list):
-            print(f"设备序号: {idx+1}, 设备名称: {device}")
+# 删除原始的ZIP文件
+os.remove(file_path)
 
-        # 用户选择要安装文件的设备
-        selected_device_idx = int(input("请选择要安装OpenShamrock的设备序号: "))
-        selected_device = devices_list[selected_device_idx - 1].split('\t')[0]
+# 列出连接到计算机上的所有安卓设备的序号和设备信息
+adb_devices = subprocess.run(['adb', 'devices'], capture_output=True, text=True).stdout
+# 检查是否存在连接的设备
+if "List of devices attached" not in adb_devices:
+    logger.info("{datetime.datetime.now()} - 未检测到已连接的设备，请连接设备后重新运行程序。")
+else:
+    devices_list = adb_devices.split('\n')[1:-2]  # 去除开头和结尾多余信息
 
-        # 使用 adb 安装解压后的文件到所选设备
-        apk_file_path = os.path.join(script_dir, extracted_file_path)
-        subprocess.run(['adb', '-s', selected_device, 'install', apk_file_path])
+# 打印设备列表供用户选择
+for idx, device in enumerate(devices_list):
+    print(f"设备序号: {idx+1}, 设备名称: {device}")
 
-        # 删除安装包
-        os.remove(apk_file_path)
+selected_device_idx = int(input("请选择要安装OpenShamrock的设备序号: "))
+while selected_device_idx not in range(1, len(devices_list) + 1):
+    print("无效的选择，请重新输入：")
+    selected_device_idx = int(input("请选择要安装OpenShamrock的设备序号: "))
+
+selected_device = devices_list[selected_device_idx - 1].split('\t')[0]
+
+# 使用 adb 安装解压后的文件到所选设备
+apk_file_path = os.path.join(script_dir, extracted_file_path)
+subprocess.run(['adb', '-s', selected_device, 'install', apk_file_path])
+
+# 删除安装包
+os.remove(apk_file_path)
